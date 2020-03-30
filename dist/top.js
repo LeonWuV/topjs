847da44d7de6257e2a29b53d283c03b4d897d1ae
乌小龙 <wuxiaolong20>
2020-03-30 22:27:00 +0800

    修改img url

--- dist/top.js
+++ dist/top.js
@@ -1,6 +1,6 @@
 /*
- * @Author: leonWu(wuxiaolong) 
- * @Date: 2018-03-22 14:01:52 
+ * @Author: leonWu(wuxiaolong)
+ * @Date: 2018-03-22 14:01:52
  * @Last Modified by: wuxiaolong
  * @Last Modified time: 2018-03-22 19:31:12
  */
@@ -10,31 +10,31 @@
         var ps = {
             w:40,
             h:40,
-            dImg:"http://olv6wm3nj.bkt.clouddn.com/18-3-22/16215533.jpg",
-            hImg:"http://olv6wm3nj.bkt.clouddn.com/18-3-22/74337023.jpg",
+            dImg:"https://raw.githubusercontent.com/LeonWuV/topjs/gh-pages/img/up-db.png",
+            hImg:"https://raw.githubusercontent.com/LeonWuV/topjs/gh-pages/img/up-h.png",
             bt:40,
             rg:30,
             s:70,
             th:300
         }
-        // merge value 
+        // merge value
         if(as !== "undefined"){
             for(var key in as){
                 ps[key] = as[key];
             }
         }
-        
+
         this.as = ps;
         this.init();
     }
     a.prototype.init = function(){
         var data  = this.as;
         var d = document.createElement("div");
-        d.style.width = data.w  + "px"; 
+        d.style.width = data.w  + "px";
         d.style.height = data.h + "px";
         d.style.position = "fixed";
         d.style.bottom = data.bt + "px"
-        d.style.right = data.rg + "px" 
+        d.style.right = data.rg + "px"
         d.style.cursor = "pointer";
         d.style.backgroundImage = "url(" + data.dImg +")";
         d.style.backgroundSize = "100%";
